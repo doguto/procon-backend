a014b9870668bc2dc62b5f748aedf33d11331baa
tsukuneA1 <tkschr1125@gmail.com>
2025-05-03 23:55:21 +0900

    update:postの取得jsonすべてにuserリレーションを含めるようにした

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::PostsController < ApplicationController
 
   def user_posts
     posts = Posts::FetchUserPostsDomain.new(user: User.find(params[:user_id])).execute
-    render json: posts.as_json(include: {user: {only: [:id,:name, :image]}})
+    render json: posts.as_json(include: { user: { only: [:id, :name, :image] } })
   end
 
   private
