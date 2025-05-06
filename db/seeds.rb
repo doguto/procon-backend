6f99449aab406fe339522e992eab5c05ca63ae06
doguto <doguto110@gmail.com>
2025-05-06 20:23:39 +0900

    Fix: seedの生成方法の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,66 +3,62 @@ require "faker"
 NUM_USERS = 10
 NUM_POSTS = 30
 NUM_FOLLOWS = 30
-NUM_LIKES = 30
-NUM_REPOSTS = 20
+NUM_LIKES = 150
+NUM_REPOSTS = 120
 
-puts "Seeding users..."
-users = NUM_USERS.times.map do
-  User.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.unique.email,
-    password: "password",
-    image: Faker::Avatar.image # ここで画像を追加
-  )
-end
+# puts "Seeding users..."
+# users = NUM_USERS.times.map do
+#   User.create!(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.unique.email,
+#     password: "password",
+#     image: Faker::Avatar.image # ここで画像を追加
+#   )
+# end
 
-puts "Seeding profiles..."
-users.each do |user|
-  user.create_profile!(
-    bio: Faker::Lorem.paragraph(sentence_count: 2)
-  )
-end
+# puts "Seeding profiles..."
+# users.each do |user|
+#   user.create_profile!(
+#     bio: Faker::Lorem.paragraph(sentence_count: 2)
+#   )
+# end
 
-puts "Seeding follow relationships..."
-follow_set = Set.new
+# puts "Seeding follow relationships..."
+# follow_set = Set.new
 
-while follow_set.size < NUM_FOLLOWS
-  follower = users.sample
-  followed = users.sample
+# while follow_set.size < NUM_FOLLOWS
+#   follower = users.sample
+#   followed = users.sample
 
-  next if follower == followed || follow_set.include?([follower.id, followed.id])
+#   next if follower == followed || follow_set.include?([follower.id, followed.id])
 
-  follower.following << followed
-  follow_set << [follower.id, followed.id]
-end
+#   follower.following << followed
+#   follow_set << [follower.id, followed.id]
+# end
 
-puts "Seeding posts..."
-NUM_POSTS.times do
-  user = users.sample
-  user.posts.create!(
-    content: Faker::Lorem.sentence(word_count: rand(5..15)),
-    status: "published",
-    created_at: Faker::Time.backward(days: 30),
-    updated_at: Time.current
-  )
-end
+# puts "Seeding posts..."
+# NUM_POSTS.times do
+#   user = users.sample
+#   user.posts.create!(
+#     content: Faker::Lorem.sentence(word_count: rand(5..15)),
+#     status: "published",
+#     created_at: Faker::Time.backward(days: 30),
+#     updated_at: Time.current
+#   )
+# end
 
 puts "Seeding likes..."
 NUM_LIKES.times do
-  user = users.sample
+  user = User.order("RANDOM()").first
   post = Post.order("RANDOM()").first
-
-  next if follower == followed || follow_set.include?([follower.id, followed.id])
 
   Like.create!(user: user, post: post)
 end
 
 puts "Seeding reposts..."
 NUM_REPOSTS.times do
-  user = users.sample
+  user = User.order("RANDOM()").first
   post = Post.order("RANDOM()").first
-
-  next if follower == followed || follow_set.include?([follower.id, followed.id])
 
   Repost.create!(user: user, post: post)
 end
