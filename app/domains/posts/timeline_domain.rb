a5713b3580f865027503ac8b6cc722c3fbd81674
doguto <doguto110@gmail.com>
2025-05-06 16:32:27 +0900

    Feat: TimelineでFrontに返す情報を追加し、Dtoにまとめた

--- app/domains/posts/timeline_domain.rb
+++ app/domains/posts/timeline_domain.rb
@@ -1,7 +1,19 @@
 module Posts
   class TimelineDomain
     def execute
-      Post.order(created_at: :desc).limit(50)
+      # 仮に現在のUserをid=1としている
+      current_user = User.find(1)
+
+      posts = Post.order(created_at: :desc).limit(50)
+      dtos = []
+      posts.each do |post|
+        is_following = Follow.exists?(follower_id: current_user.id, followed_id: post.user.id)
+
+        dto = TimelineDto.new(post: post, like_num: 1, repost_num: 2, reply_num: 0, is_following: is_following)
+        dtos.push(dto)
+      end
+
+      dtos
     end
   end
 end
