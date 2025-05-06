a0a3f864ad354f5646794a100bfea9482d4e84d4
doguto <doguto110@gmail.com>
2025-05-06 20:24:57 +0900

    Feat: LikeとRepostのテーブルをTimelineに適用

--- app/domains/posts/timeline_domain.rb
+++ app/domains/posts/timeline_domain.rb
@@ -7,9 +7,11 @@ module Posts
       posts = Post.order(created_at: :desc).limit(50)
       dtos = []
       posts.each do |post|
+        likes_num = Like.where(post_id: post.id).count
+        reposts_num = Repost.where(post_id: post.id).count
         is_following = Follow.exists?(follower_id: current_user.id, followed_id: post.user.id)
 
-        dto = TimelineDto.new(post: post, like_num: 1, repost_num: 2, reply_num: 0, is_following: is_following)
+        dto = TimelineDto.new(post: post, like_num: likes_num, repost_num: reposts_num, reply_num: 0, is_following: is_following)
         dtos.push(dto)
       end
 
