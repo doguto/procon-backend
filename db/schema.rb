d994470093affaa783e56d7ee37c8af118cf33c7
doguto <doguto110@gmail.com>
2025-05-14 21:22:49 +0900

    Fix: AIPostDomainがエラー吐いてたので修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[8.0].define(version: 2025_05_08_132333) do
-
+ActiveRecord::Schema[8.0].define(version: 20_250_508_132_333) do
   create_table "ai_users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
