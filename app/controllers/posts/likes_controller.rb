e78959997ecbf6a7e7275835ac23efe93a7d53fa
doguto <doguto110@gmail.com>
2025-05-18 13:30:40 +0900

    Feat: Like機能の仮実装

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,6 +1,8 @@
 class Posts::LikesController < ApplicationController
   def create
     domain = UserLikeDomain.new
-    domain.execute(user_id: params[:user_id], post_id: params[:post_id])
+    successon = domain.execute(user_id: params[:user_id], post_id: params[:post_id])
+
+    render json: { message: "Success" }, status: :ok if successon
   end
 end
--- app/domains/posts/user_like_domain.rb
+++ app/domains/posts/user_like_domain.rb
@@ -3,9 +3,9 @@ class UserLikeDomain < ApplicationDomain
     like = UserLike.find_by(user_id: user_id, post_id: post_id)
     if like
       like.destroy
-
     else
       UserLike.create(user_id: user_id, post_id: post_id)
     end
+    true
   end
 end
