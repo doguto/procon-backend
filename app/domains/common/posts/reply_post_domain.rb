273984012f9c1dfae43ed13fb992efb3771e8ca5
doguto <doguto110@gmail.com>
2025-05-19 13:46:56 +0900

    Fix: Dtoを返すようにReplyを修正

--- app/domains/common/posts/reply_post_domain.rb
+++ app/domains/common/posts/reply_post_domain.rb
@@ -9,7 +9,9 @@ module Common::Posts
       user = User.find(@user_id)
       post = Post.new(user: user, content: content, reply_to_id: reply_to_id)
       if post.save
-        post
+        post.map do |p|
+          PostDto.new(p)
+        end
       else
         raise DomainError, post.errors.full_messages.join(", ")
       end
