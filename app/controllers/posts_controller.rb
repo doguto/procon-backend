b22d53448262fe8178e5f21ba8dcb222f5cdfe48
doguto <doguto110@gmail.com>
2025-05-19 13:46:20 +0900

    Fix: camelizeするように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,19 @@
 class PostsController < ApplicationController
   def index
-    dtos = Page::HomePage::TimelineDomain.new.execute
-    render json: dtos.as_json
+    dtos = Page::HomePage::TimelineDomain.new.execute(set_current_user.id)
+    render json: dtos.map { |dto| dto.get.to_camelized_json }.as_json
   end
 
   def show
     post = Common::Posts::ShowPostDomain.new(id: params[:id]).execute
-    render json: post
+    render json: post.get.to_camelized_json
   rescue ActiveRecord::RecordNotFound
-    render json: { error: "Post not fount" }, status: :not_found
+    render json: { error: "Post not found" }, status: :not_found
   end
 
   def create
     post = Common::Posts::UserPostDomain.new(user_id: params[:post][:user_id]).execute(content: params[:post][:content])
-    render json: post, status: :created
+    render json: post.get.to_camelized_json, status: :created
   rescue ActiveRecord::RecordNotFound
     render json: { error: "User not found" }, status: :not_found
   rescue StandardError => e
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
 
   def user_posts
     posts = Common::Posts::FetchUserPostsDomain.new(user_id: params[:user_id]).execute
-    render json: posts
+    render json: posts.map { |post| post.get.to_camelized_json }.as_json
   end
 
   private
