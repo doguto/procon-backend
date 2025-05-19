6a1a965e367c9e9978a7fb8ef877f4e0fd060292
doguto <doguto110@gmail.com>
2025-05-19 11:47:55 +0900

    Feat: 初期化時に拡張機能は自動で読み込むように変更

--- test/controllers/camerize_test.rb
+++ test/controllers/camerize_test.rb
@@ -1,4 +1,4 @@
 require 'active_support'
 require 'active_support/core_ext/string/inflections'
 
-puts({ "foo_foo" => "bar_jo", "baz_rin" => 12 }.transform_keys { |key| key.to_s.camelize(:lower) }) if __FILE__ == $0
+puts({ "foo_foo" => "bar_jo", "baz_rin" => 12 }.camelize)  if __FILE__ == $0
