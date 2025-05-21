8a9813bfda869712b5c4cc6fa5a5b0fbbcfcedd5
doguto <doguto110@gmail.com>
2025-05-21 21:36:01 +0900

    Fix: hashの宣言の修正

--- app/datatransfers/post_detail_dto.rb
+++ app/datatransfers/post_detail_dto.rb
@@ -1,27 +1,28 @@
 class PostDetailDto
-  def initialize(post, current_user = nil)
+  def initialize(post:, current_user:)
     @post = post
     @current_user = current_user
   end
 
   def get
     {
-      id => @post.id,
-      content => @post.content,
-      created_at => @post.created_at,
-      updated_at => @post.updated_at,
-      user_id => @post.user_id,
-      reply_to_id => @post.reply_to_id,
-      user => {
-        id => @post.user.id,
-        name => @post.user.name,
-        image => @post.user.image
-      },
-      replies_count => @post.replies.count,
-      likes_count => @post.likes.count,
-      reposts_count => @post.reposts.count,
-      replies => @post.replies.order(created_at: :asc).map { |post| PostDetailDto.new(post, @current_user).get }
-    }
+      "id" => @post.id,
+      "content" => @post.content,
+      "created_at" => @post.created_at,
+      "updated_at" => @post.updated_at,
+      "user_id" => @post.user_id,
+      "reply_to_id" => @post.reply_to_id,
+      "user" => {
+        "id" => @post.user.id,
+        "name" => @post.user.name,
+        "image" => @post.user.image,
+        "is_following" => following_user?
+      }.camelize,
+      "replies_count" => @post.replies.count,
+      "likes_count" => @post.likes.count,
+      "reposts_count" => @post.reposts.count,
+      "replies" => @post.replies.order(created_at: :asc).map { |post| PostDetailDto.new(post, @current_user).get }
+    }.camelize
   end
 
   private
@@ -29,6 +30,6 @@ class PostDetailDto
   def following_user?
     return false unless @current_user
 
-    @current_user.following.exists?(id: @post.user.id)
+    @current_user.following.exists?(id: @post.user_id)
   end
 end
