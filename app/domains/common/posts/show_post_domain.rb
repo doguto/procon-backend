144e441e1a56a52998626fa50ffde192501e8204
tsukuneA1 <tkschr1125@gmail.com>
2025-05-21 21:35:33 +0900

    fix:認証処理部分を書き換えた

--- app/domains/common/posts/show_post_domain.rb
+++ app/domains/common/posts/show_post_domain.rb
@@ -1,13 +1,14 @@
 module Common::Posts
   class ShowPostDomain < ApplicationDomain
-    def initialize(id:)
+    def initialize(id:, current_user:)
       super()
       @id = id
+      @current_user = current_user
     end
 
     def execute
       post = Post.includes(:user, :replies, :likes, :reposts).find(@id)
-      PostDetailDto.new(post)
+      PostDetailDto.new(post, @current_user)
     end
   end
 end
